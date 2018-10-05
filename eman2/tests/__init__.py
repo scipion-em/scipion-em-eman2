4a74e267869a692670f52e9412c7fda1ad8851de
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2018-10-05 14:52:58 +0200

    Add a pip modules files

---
+++ eman2/tests/__init__.py
@@ -0,0 +1,6 @@
+
+from test_protocols_eman import TestEmanBase, TestEmanAutopick, \
+    TestEmanCtfAuto, TestEmanInitialModelGroel, TestEmanInitialModelMda, \
+    TestEmanReconstruct, TestEmanRefine2D, TestEmanRefine2DBispec, \
+    TestEmanRefineEasy, TestEmanTiltValidate
+
