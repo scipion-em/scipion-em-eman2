af05cedcf3882d4ed4b4efd51cf3d02761ce0e0c
Adrian Quintana <adrianquintana@gmail.com>
2019-05-07 11:07:48 +0100

    fix import

--- eman2/protocols/__init__.py
+++ eman2/protocols/__init__.py
@@ -36,7 +36,8 @@ from protocol_autopick_boxer import EmanProtAutopick
 from protocol_autopick_sparx import SparxGaussianProtPicking
 
 try:
-    from protocol_tomo_boxing import EmanProtTomoBoxing, EmanProtTomoExtraction
+    from protocol_tomo_boxing import EmanProtTomoBoxing
+    from protocol_tomo_extraction import EmanProtTomoExtraction
 except ImportError:
     raise ImportError(
         'To use a Tomography protocol scipion-em-tomo plugin is required.'
