0230221f2e7f34a1b8978f6a48e589ed062d670f
idelcano <ignaciodelcano@gmail.com>
2019-05-09 22:49:09 +0200

    added all refinement params

--- eman2/protocols/protocol_tomo_subtomogram_refinement.py
+++ eman2/protocols/protocol_tomo_subtomogram_refinement.py
@@ -25,19 +25,20 @@
 # *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
+from types import NoneType
+
+from xmipp3.convert import writeSetOfParticles
 
 import pyworkflow.protocol.params as params
 import pyworkflow.em as pwem
 
 from tomo.protocols import ProtTomoBase
 from pyworkflow.protocol import STEPS_PARALLEL, join
-
+from pprint import pprint
 import eman2
 from eman2.constants import *
 
 from tomo.objects import SetOfSubTomograms, SubTomogram
-# Micrograph type constants for particle extraction
-from pyworkflow.utils.dataset import DataSet
 
 SAME_AS_PICKING = 0
 class EmanProtTomoRefinement(pwem.EMProtocol, ProtTomoBase):
@@ -58,12 +59,12 @@ class EmanProtTomoRefinement(pwem.EMProtocol, ProtTomoBase):
                       important=True, label='Input SetOfSubTomograms',
                       help='Select the SetOfSubTomograms.')
         form.addParam('inputRef', params.PointerParam,
-                      pointerClass='Volume', allowsNull=True,
+                      pointerClass='Volume',
                       default=None, label='Input Ref Tomogram',
                       help='3D reference for initial model generation.'
                            'No reference is used by default.')
 
-        group = form.addGroup('Input')
+        group = form.addGroup('Config')
         group.addParam('niter', pwem.IntParam, default=5,
                        label='Number of iterations',
                        help='The number of iterations to perform.')
@@ -80,75 +81,73 @@ class EmanProtTomoRefinement(pwem.EMProtocol, ProtTomoBase):
         group.addParam('goldstandard', pwem.IntParam, default=-1,
                        label='GoldStandard:',
                        help='initial resolution for gold standard refinement')
+        group.addParam('goldcontinue', params.BooleanParam, default=False,
+                       label='Gold continue',
+                       help='continue from an existing gold standard refinement')
+        group.addParam('maskFile', params.PointerParam, allowsNull=True,
+                       pointerClass='VolumeMask', label='Mask file',
+                       help='Select the mask object')
+        group.addParam('setsf', params.PointerParam, allowsNull=True,
+                       pointerClass='VolumeMask', label='Structure factor',
+                       help='Select the structure factor')
+        group.addParam('sym', params.StringParam, default='c1',
+                       label='Symmetry',
+                      help='Symmetry (Default: c1')
+        group.addParam('localfilter', params.BooleanParam, default=False,
+                       label='Local filter',
+                      help='use tophat local')
+        group.addParam('maxtilt', params.FloatParam, default=90.0,
+                       label='maxtilt',
+                       help='Explicitly zeroes data beyond specified tilt angle.'
+                            'Assumes tilt axis exactly on Y and zero tilt in X-Y'
+                            'plane. Default 90 (no limit).')
+
         form.addParallelSection(threads=2, mpi=4)
 
     #--------------- INSERT steps functions ----------------
 
+
     def _insertAllSteps(self):
+        #TODO: Get the basename.hdf from the inputSetOfSubTomogram
+        #self._insertFunctionStep('convertInputStep')
+        print('#--------------- convertInputStep -----------------------')
         self._insertFunctionStep('refinementSubtomogram')
-
-
-    def runImportParticlesSqlite(cls, pattern, samplingRate):
-        """ Run an Import particles protocol. """
-        cls.protImport = cls.newProtocol(pwem.ProtImportParticles,
-                                         importFrom=4,
-                                         sqliteFile=pattern, samplingRate=samplingRate)
-        cls.launchProtocol(cls.protImport)
-        # check that input images have been imported (a better way to do this?)
-        if cls.protImport.outputParticles is None:
-            raise Exception('Import of images: %s, failed. outputParticles is None.' % pattern)
-        return cls.protImport
-
-    def getFile(self, key):
-        if key in self.filesDict:
-            return join(self.path, self.filesDict[key])
-        return join(self.path, key)
+        #TODO: Set and show the output
+        #self._insertFunctionStep('createOutputStep')
 
     #--------------- STEPS functions -----------------------
     def refinementSubtomogram(self):
-        print ('---------------------------------------------------------------------------------')
-        print ('refinementSubtomogram')
-        print ('---------------------------------------------------------------------------------')
-
-
-        print("import particles")
-
-        self.partsFn = self.getFile(self.inputSetOfSubTomogram.get().getFileName())
-        self.protImport = self.runImportParticlesSqlite(self.partsFn, 3.5)
-        args = ' %s' % (
-            pwem.os.getcwd() + "/" + self.inputSetOfSubTomogram.get().getFileName()).replace("subtomograms.sqlite", "extra/sptboxer_01/basename.hdf")
-        print(self.inputSetOfSubTomogram.get().get())
-        print ('---------------------------------------------------------------------------------')
-        print(self.inputRef.get())
-        if self.inputRef is not None:
+        """ Run the Subtomogram refinement. """
+        input = pwem.os.getcwd() + "/" + self.inputSetOfSubTomogram.get().getFileName().replace("subtomograms.sqlite","extra/sptboxer_01/basename.hdf")
+        print('#--------------- refinementSubtomogram ----------------------- %s ' % input)
+        args = ' %s' % input
+        if not isinstance(self.inputRef.get(), NoneType):
             args += (' --reference=%s ' % self.inputRef.get().getFileName())
-
-        if self.mass:
-            args += (' --mass=%f' % self.mass)
-
-        if self.niter > 1:
-            args += ' --niter=%d' % self.niter
-
+        args += (' --mass=%f' % self.mass)
         args += ' --threads=%d' % self.threads
         args += ' --goldstandard=%d ' % self.goldstandard
         args += ' --pkeep=%f ' % self.pkeep
-
-        args += ' --sym=c1 --maxtilt=90.0 '
-        print ('---------------------------------------------------------------------------------')
-        print("command: " + args)
+        args += ' --sym=%s ' % self.sym
+        args += ' --maxtilt=%s ' % self.maxtilt
+        if self.niter > 1:
+            args += ' --niter=%d' % self.niter
+        if self.goldcontinue:
+            args += ' --goldcontinue '
+        if self.localfilter:
+            args += ' --localfilter '
+        print('#--------------- STEPS functions -----------------------')
+        print("command: e2spt_refine.py " + args)
+        print('#--------------- STEPS functions -----------------------')
         program = eman2.Plugin.getProgram('e2spt_refine.py')
         self.runJob(program, args,
                     cwd=self._getExtraPath())
-
-    def convertInputStep(self):
-        pass
 
     def runMLStep(self, params):
         pass
 
     def createOutputStep(self):
         pass
-
+#
     #--------------- INFO functions -------------------------
 
     def _validate(self):
@@ -167,10 +166,13 @@ class EmanProtTomoRefinement(pwem.EMProtocol, ProtTomoBase):
         summary = []
         summary.append("SetOfSubTomograms source: %s" % (self.inputSetOfSubTomogram.get().getFileName()))
 
+        if not isinstance(self.inputRef.get(), NoneType):
+            summary.append("Referenced Tomograms source: %s" % (self.inputRef.get().getFileName()))
+
         if self.getOutputsSize() >= 1:
-            summary.append("Subtomogram refinamented")
+            summary.append("Subtomogram Averaging Completed")
         else:
-            summary.append("Output subtomograms not ready yet.")
+            summary.append("Subtomogram Averaging not ready yet.")
 
         return summary
 
