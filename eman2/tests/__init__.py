42868a3b2ebf4f23dcc145df4c29bdd2e2c5a4d1
idelcano <ignaciodelcano@gmail.com>
2019-06-05 16:05:24 +0200

    added eman2 tests resources

--- eman2/tests/__init__.py
+++ eman2/tests/__init__.py
@@ -5,3 +5,12 @@ from test_protocols_eman import (TestEmanBase, TestEmanAutopick,
                                  TestEmanRefine2D, TestEmanRefine2DBispec,
                                  TestEmanRefineEasy, TestEmanTiltValidate)
 
+
+
+from pyworkflow.tests import DataSet
+
+DataSet(name='tomo-em', folder='tomo-em',
+        files={
+               'tomo1': 'overview_wbp.em',
+               'tomo2': 'overview_wbp2.em',
+               'eman_coordinates': 'coordinates3Deman2'})
