bf9bfac9e3c11e2beb6409a5f1c450d108f771d6
Adrian Quintana <adrianquintana@gmail.com>
2019-11-28 15:25:24 +0000

    better error handling on init

--- eman2/protocols/__init__.py
+++ eman2/protocols/__init__.py
@@ -24,6 +24,8 @@
 # *
 # **************************************************************************
 
+
+
 from protocol_boxing import EmanProtBoxing
 from protocol_ctf import EmanProtCTFAuto
 from protocol_initialmodel import EmanProtInitModel
@@ -42,7 +44,8 @@ try:
     from protocol_tomo_extraction import EmanProtTomoExtraction
     from protocol_tomo_subtomogram_refinement import EmanProtTomoRefinement
     from protocol_tomo_initialmodel import EmanProtTomoInitialModel
-except ImportError:
-    raise ImportError(
-        'To use a Tomography protocol scipion-em-tomo plugin is required.'
-        ' See https://github.com/scipion-em/scipion-em-tomo for further details')
+except ImportError as importError:
+    from pyworkflow.utils import pluginNotFound
+    pluginNotFound("eman2", 'Tomography protocols couldn\'t be imported. To use a Tomography protocol '
+                            'scipion-em-tomo plugin is required. '
+                            ' See https://github.com/scipion-em/scipion-em-tomo for further details')
