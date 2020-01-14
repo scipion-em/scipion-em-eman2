432f4fbe3702ed4962f67bdeabcdb096fa65fc3b
JorMaister <jorgejjs86@gmail.com>
2020-01-14 10:52:04 +0100

    Add entry point

--- setup.py
+++ setup.py
@@ -180,11 +180,9 @@ setup(
     #
     # For example, the following would provide a command called `sample` which
     # executes the function `main` from this package when invoked:
-    # entry_points={  # Optional
-    #    'console_scripts': [
-    #        'sample=sample:main',
-    #    ],
-    # },
+    entry_points={  # Optional
+       'pyworkflow.plugin': 'eman2 = eman2',
+    },
 
     # List additional URLs that are relevant to your project as a dict.
     #
