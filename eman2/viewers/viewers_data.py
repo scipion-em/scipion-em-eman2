403153a8bd6a98a3eca962c72e66f1df15fda71a
DavidHerreros <david.herreroscalero@gmail.com>
2020-12-14 12:40:28 +0100

    Eman viewers save temporary files to extra path

--- eman2/viewers/viewers_data.py
+++ eman2/viewers/viewers_data.py
@@ -68,7 +68,7 @@ class EmanDataViewer(pwviewer.Viewer):
 
             tomoList = [item.clone() for item in outputCoords.getPrecedents().iterItems()]
 
-            path = os.path.join(self.protocol._getTmpPath(), '..')
+            path = os.path.join(self.protocol._getExtraPath(), '..')
 
             tomoProvider = TomogramsTreeProvider(tomoList, path, 'json',)
 
