984cbf92bdea473328b678944e355ad0e51d60c8
JorMaister <jorgejjs86@gmail.com>
2023-06-07 14:32:28 +0200

    Add some code to cover TODOS 2 and 3

--- eman2/protocols/protocol_gmm_refine_iter.py
+++ eman2/protocols/protocol_gmm_refine_iter.py
@@ -24,16 +24,19 @@
 # **************************************************************************
 
 from enum import Enum
-from os.path import basename, abspath
+from os.path import basename, abspath, join
 
+from pwem import ALIGN_PROJ, ALIGN_NONE
 from pyworkflow.object import String
 from pyworkflow.utils import Message
-from pyworkflow.utils.path import createLink
+from pyworkflow.utils.path import createLink, makePath
 from pyworkflow.constants import BETA
 from pyworkflow.protocol.params import (PointerParam, StringParam, FloatParam)
 from pwem.protocols import EMProtocol
 from pwem.objects.data import Volume, AtomStruct
 from .. import Plugin
+import logging
+logger = logging.getLogger(__name__)
 
 
 class outputs(Enum):
@@ -66,6 +69,10 @@ class EmanProtGmmRefineIter(EMProtocol):
 
     def _defineParams(self, form):
         form.addSection(label=Message.LABEL_INPUT)
+        form.addParam('inParticles', PointerParam,
+                      pointerClass='SetOfParticles',
+                      label='Particles',
+                      important=True)
         form.addParam('initRef', PointerParam,
                       pointerClass='Volume',
                       label="Reference volume",
@@ -89,27 +96,51 @@ class EmanProtGmmRefineIter(EMProtocol):
     # --------------------------- INSERT steps functions ----------------------
 
     def _insertAllSteps(self):
+        self._initialize()
         self._insertFunctionStep(self.convertInputStep)
         self._insertFunctionStep(self.runGmmRefineIterStep)
         self._insertFunctionStep(self.createOutputStep)
 
     # --------------------------- STEPS functions -----------------------------
+    def _initialize(self):
+        self.refFile = self._getExtraPath(basename(self.initRef.get().getFileName()))
+
     def convertInputStep(self):
-        """Convert the reference to HDF if necessary"""
+        # Convert the reference to HDF if necessary
         program = Plugin.getProgram('e2proc3d.py')
         inRef = self.initRef.get()
         inFile = inRef.getFileName()
-        outFileName = basename(inFile)
         sRate = inRef.getSamplingRate()
         if inFile.endswith('.hdf'):
-            createLink(inFile, self._getExtraPath(outFileName))
+            createLink(inFile, self.refFile)
         else:
-            args = '%s %s --apix %.2f ' % (abspath(inFile), outFileName, sRate)
+            args = '%s %s --apix %.2f ' % (abspath(inFile), self.refFile, sRate)
             self.runJob(program, args, cwd=self._getExtraPath())
-        self.refFile = outFileName
 
-        # TODO 3 (Jorge): convert Scipion set to star file. Same as here --> https://raw.githubusercontent.com/scipion-em/scipion-em-cryosparc2/devel/cryosparc2/convert/convert.py#:~:text=logger.info(%27Trying%20to%20generate%20the%20star%20file%20with%20Relion,fileName%2C%20**args)%0A%20%20%20%20%20%20%20%20logger.info(%27The%20star%20file%20was%20generate%20successfully%20...%27)
-        # TODO 4 (Muyuan / Jorge): call e2convertrelion.py to get the Eman format (check relion SPA plugin)
+        # Generate a star file using the input particles
+        starFile = self._getExtraPath('inParticles.star')
+        inParticles = self.inParticles.get()
+        self.particles2Star(inParticles, starFile)
+
+        # Use EMAN to generate a .lst file with the location of all particles and their initial orientation assignment
+        program = Plugin.getProgram('e2convertrelion.py')
+        acquisition = inParticles.getAcquisition()
+        particlesDir = 'particles'
+        makePath(self._getExtraPath(particlesDir))
+        args = [
+            f'{starFile}',
+            f'--voltage {acquisition.getVoltage()}',
+            f'--cs {acquisition.getSphericalAberration():.2f}',
+            f'--apix {inParticles.getSamplingRate():.3f}',
+            f'--amp {acquisition.getAmplitudeContrast():-2f}',
+            f'--onestack {join(particlesDir, "particles_all.hdf")}',
+            f'--sym {self.symmetry.get()}',
+            '--make3d'
+        ]
+        self.runJob(program, args, cwd=self._getExtraPath())
+
+        # TODO 3 (Jorge) [DONE]: convert Scipion set to star file. Same as here --> https://raw.githubusercontent.com/scipion-em/scipion-em-cryosparc2/devel/cryosparc2/convert/convert.py#:~:text=logger.info(%27Trying%20to%20generate%20the%20star%20file%20with%20Relion,fileName%2C%20**args)%0A%20%20%20%20%20%20%20%20logger.info(%27The%20star%20file%20was%20generate%20successfully%20...%27)
+        # TODO 4 (Muyuan / Jorge) [DONE]: call e2convertrelion.py to get the Eman format (check relion SPA plugin)
 
     def runGmmRefineIterStep(self):
         # TODO 2 (Muyuan): finish the arguments generation for the calling command to EMAN (method _getGmmRefineArgs)
@@ -153,3 +184,15 @@ class EmanProtGmmRefineIter(EMProtocol):
         if atomStruct:
             args.append(f'--initpts {atomStruct.getFileName()}')
         return ' '.join(args)
+
+    def particles2Star(self, imgSet, fileName):
+        logger.info('Trying to generate the star file with Relion convert...')
+        args = {'outputDir': self._getExtraPath(),
+                'fillMagnification': True,
+                'fillRandomSubset': True}
+        from relion import convert
+        alignType = ALIGN_PROJ if imgSet.hasAlignmentProj() else ALIGN_NONE
+        args['alignType'] = alignType
+        args['incompatibleExtensions'] = ['hdf', 'stk']
+        convert.writeSetOfParticles(imgSet, fileName, **args)
+        logger.info('The star file was generate successfully ...')
